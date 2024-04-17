974e63e809c7c195f6ca44b406cb855d20edbe42
Nadra Zaki Saputra <149183410+cey0@users.noreply.github.com>
2024-04-17 20:49:50 +0700

    auth done

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,11 +1,50 @@
+const bcrypt = require('bcrypt');
 const User = require('../schema/AuthSchema');
+const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
   try{
-    const user = new User(req.body);
+    const {password, ...rest} = req.body;
+
+    const existUserEmail = await User.findOne({email: rest.email});
+    if(existUserEmail){
+      return res.status(400).json({message: 'email has already taken'});
+    }
+
+    const existUserUN = await User.findOne({username: rest.username});
+    if(existUserUN){
+      return res.status(400).json({message: 'username has already taken'});
+    }
+
+    const existUserPhone = await User.findOne({phone: rest.phone});
+    if(existUserPhone){
+      return res.status(400).json({message: 'phone has already taken'});
+    }
+
+    const hashPass = await bcrypt.hash(password, 10);
+    const user = new User({...rest, password: hashPass});
     await user.save();
     res.status(201).json({message: 'User created successfully'})
   }catch(err){
     res.status(500).json({message: err.message});
   }
 };
+
+exports.Login = async (req,res) => {
+  try{
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(404).json({message: 'User not found'});
+    }else{
+      const token = jwt.sign({
+        nama: user.nama,
+        id: user._id
+      }, process.env.SECRET_KEY, {expiresIn: '1h'});
+      return res.status(200).json({message: 'Login success', token});
+    }
+  }catch(err){
+    res.status(500).json({message: err.message});
+  }
+};
+
--- src/router/AuthRouter.js
+++ src/router/AuthRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controller/AuthController');
 
-router.post('/', UserController.register);
+router.post('/register', UserController.register);
+router.post('/login',UserController.Login);
 
 module.exports = router;
 
